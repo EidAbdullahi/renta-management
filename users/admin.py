f52362c34b883e8e427bb65ea7200c0f7413232a
Eid Abdulla <eidabdullahi10.com>
2025-05-20 20:19:54 +0200

    commit chnages

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Partner
 from .models import  ForSaleProperty, CommercialProperty,VacantRoom
+from django.utils.html import format_html
+
 
 # Register your models here.
 admin.site.register(Partner)
@@ -24,3 +26,11 @@ class VacantRoomAdmin(admin.ModelAdmin):
     )
     list_filter = ('room_type', 'location', 'is_available')
     search_fields = ('title', 'description', 'location')
+    
+    readonly_fields = ['video_preview']
+
+    def video_preview(self, obj):
+        if obj.video_tour:
+            return format_html(f'<video width="320" height="240" controls><source src="{obj.video_tour.url}" type="video/mp4">Your browser does not support the video tag.</video>')
+        return "No video"
+    video_preview.short_description = "Video Tour Preview"
