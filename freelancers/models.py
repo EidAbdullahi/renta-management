0ac91ec11f808c3f320d16cb7e808fe9a7964d42
Eid Abdulla <eidabdullahi10.com>
2025-05-20 21:49:39 +0200

    commit changes

--- freelancers/models.py
+++ freelancers/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from cloudinary.models import CloudinaryField  # Make sure this import is included
 
 CATEGORY_CHOICES = [
     ('Plumber', 'Plumber'),
@@ -7,18 +6,25 @@ CATEGORY_CHOICES = [
     ('Cleaner', 'Cleaner'),
     ('Caretaker', 'Caretaker'),
     ('Handyman', 'Handyman'),
-    ('Movers', 'Movers'),  # Capitalized for consistency
+    ('movers', 'movers'),
+
 ]
 
 class Freelancer(models.Model):
     name = models.CharField(max_length=100)
-    photo = CloudinaryField('image', null=True, blank=True)  # Replaces ImageField
+    photo = models.ImageField(upload_to='freelancers/')
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     description = models.TextField()
     contact = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    video = CloudinaryField('video', null=True, blank=True)  # Replaces FileField
+    video = models.FileField(upload_to='freelancers/videos/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    freelancer = models.ForeignKey(Freelancer, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
